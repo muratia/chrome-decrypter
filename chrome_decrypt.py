7b92c5aa5411459dd0469aac3d1101c7f737d114
amurati <ahmet.murati@live.com>
2017-10-23 13:09:08 +0200

    Updates
    Removed closing of Chrome to read data
    Removed listing the data in the screen
    Added exporting the data into a comma separated values file

--- chrome_decrypt.py
+++ chrome_decrypt.py
@@ -2,10 +2,19 @@ import os
 import sqlite3
 import win32crypt
 import sys
+from shutil import copyfile
+import csv
 
-# killing of Chrome browser in Windows OS
-os.system("taskkill /im Chrome.exe /f") 
- 
+
+ #os.system("taskkill /im Chrome.exe /f")
+
+def csv_writer(data, path):
+    if len(data)>1:
+        with open(path, "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for line in data:
+                writer.writerow(line)
+rows = []
 try:
     path = sys.argv[1]
 except IndexError:
@@ -16,7 +25,8 @@ except IndexError:
 # Connect to the Database
 try:
     print('[+] Opening ' + path)
-    conn = sqlite3.connect(path)
+    copyfile(path,  "Login_data")
+    conn = sqlite3.connect("Login_data")
     cursor = conn.cursor()
 except Exception as e:
     print('[-] %s' % (e))
@@ -28,7 +38,7 @@ try:
 except Exception as e:
     print('[-] %s' % (e))
     sys.exit(1)
-
+i = 0
 data = cursor.fetchall()
 header = ['URL', 'Username', 'Password'];
 if len(data) > 0:
@@ -40,7 +50,18 @@ if len(data) > 0:
             print('[-] %s' % (e))
             pass
         if password:
-            print('''[+] URL: %s  Username: %s Password: %s''' % (result[0], result[1], password))
+           # print('''[+] URL: %s  Username: %s Password: %s''' % (result[0], result[1], password))
+            url = result[0];
+            username = result[1]
+            pwd = password.decode()
+            rows.append(["URL: "  + url ," Username: " + username , " Password: " + pwd + ";"])
+            i+=1
+
 else:
     print('[-] No results returned from query')
     sys.exit(0)
+
+conn.close()
+csv_writer(rows, "passwords.csv")
+print("Exported "+ str(i) + " rows")
+os.remove("Login_data")
